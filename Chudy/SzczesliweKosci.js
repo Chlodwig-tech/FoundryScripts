150ea4f382a1253864585a32c2e0ddb19c4dcf5e
Chlodwig-tech <80710710+Chlodwig-tech@users.noreply.github.com>
2024-10-13 23:26:03 +0200

    Update SzczesliweKosci.js

--- Chudy/SzczesliweKosci.js
+++ Chudy/SzczesliweKosci.js
@@ -14,24 +14,31 @@ if (window.chudyMacroExecuted) {
 
     function chudy_hook(testData, cardOptions) {
         var target = testData.result.target;
+        var old_roll = testData.result.roll;
         var roll = chudy_change_roll(testData.result.roll);
         var SL = Math.floor(target / 10) - Math.floor(roll / 10) + testData.result.slBonus;
 
         testData.result.roll = roll;
         testData.result.SL = SL;
 
-        const descriptions = [
-            { min: 6, description: 'Zdumiewający Sukces. "Tak! Idealnie!"' },
-            { min: 4, description: 'Imponujący Sukces. "Tak, i..."' },
-            { min: 2, description: 'Sukces. "Tak."' },
-            { min: 0, description: 'Znikomy Sukces. "Tak, ale..."' },
-            { min: -1, description: 'Znikoma Porażka. "Nie, ale..."' },
-            { min: -3, description: 'Porażka. "Nie."' },
-            { min: -5, description: 'Krytyczna Porażka. "Nie, i..."' },
-            { min: -Infinity, description: 'Zdumiewająca Porażka. "Nie, nie ma mowy!"' }
-        ];
+        if(SL === 0 && roll > target){
+            testData.result.description = `Znikoma Porażka. "Nie, ale..." (${old_roll})`;
+        }else{
+            const descriptions = [
+                { min: 6, description: 'Zdumiewający Sukces. "Tak! Idealnie!"' },
+                { min: 4, description: 'Imponujący Sukces. "Tak, i..."' },
+                { min: 2, description: 'Sukces. "Tak."' },
+                { min: 0, description: 'Znikomy Sukces. "Tak, ale..."' },
+                { min: -1, description: 'Znikoma Porażka. "Nie, ale..."' },
+                { min: -3, description: 'Porażka. "Nie."' },
+                { min: -5, description: 'Krytyczna Porażka. "Nie, i..."' },
+                { min: -Infinity, description: 'Zdumiewająca Porażka. "Nie, nie ma mowy!"' }
+            ];
+    
+            testData.result.description = `${descriptions.find(d => SL >= d.min).description} (${old_roll})`;
+        }
 
-        testData.result.description = descriptions.find(d => SL >= d.min).description;
+        
     }
 
     Hooks.on('wfrp4e:rollTest', chudy_hook);
