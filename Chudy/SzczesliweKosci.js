1bbe18291ffd960aa6f0439062c21cd31c78809f
Chlodwig-tech <80710710+Chlodwig-tech@users.noreply.github.com>
2024-10-13 23:16:37 +0200

    Update SzczesliweKosci.js

--- Chudy/SzczesliweKosci.js
+++ Chudy/SzczesliweKosci.js
@@ -1,36 +1,40 @@
-function chudy_change_roll(result){
-    if(result >= 90 && result < 100){
-        result -= 80;
+if (window.chudyMacroExecuted) {
+    ui.notifications.warn("Szczęśliwe kości Chudego zostały już zainstalowane.");
+} else {
+
+    function chudy_change_roll(result) {
+        if (result >= 90 && result < 100) {
+            result -= 80;
+        }
+        if (result % 10 === 1) {
+            result = parseInt(8) + parseInt(result);
+        }
+        return result;
     }
-    if(result % 10 === 1){
-        result = parseInt(8) + parseInt(result);
+
+    function chudy_hook(testData, cardOptions) {
+        var target = testData.result.target;
+        var roll = chudy_change_roll(testData.result.roll);
+        var SL = Math.floor(target / 10) - Math.floor(roll / 10) + testData.result.slBonus;
+
+        testData.result.roll = roll;
+        testData.result.SL = SL;
+
+        const descriptions = [
+            { min: 6, description: 'Zdumiewający Sukces. "Tak! Idealnie!"' },
+            { min: 4, description: 'Imponujący Sukces. "Tak, i..."' },
+            { min: 2, description: 'Sukces. "Tak."' },
+            { min: 0, description: 'Znikomy Sukces. "Tak, ale..."' },
+            { min: -1, description: 'Znikoma Porażka. "Nie, ale..."' },
+            { min: -3, description: 'Porażka. "Nie."' },
+            { min: -5, description: 'Krytyczna Porażka. "Nie, i..."' },
+            { min: -Infinity, description: 'Zdumiewająca Porażka. "Nie, nie ma mowy!"' }
+        ];
+
+        testData.result.description = descriptions.find(d => SL >= d.min).description;
     }
-    return result;
+
+    Hooks.on('wfrp4e:rollTest', chudy_hook);
+    window.chudyMacroExecuted = true;
+    ui.notifications.info("Szczęśliwe kości Chudego zainstalowano pomyślnie");
 }
-
-
-function chudy_hook(testData, cardOptions){
-    var target = testData.result.target;
-    var roll = chudy_change_roll(testData.result.roll);
-    var SL = Math.floor(target / 10) - Math.floor(roll / 10) + testData.result.slBonus;
-
-    testData.result.roll = roll;
-    testData.result.SL = SL;
-
-    const descriptions = [
-        { min: 6, description: 'Zdumiewający Sukces. "Tak! Idealnie!"' },
-        { min: 4, description: 'Imponujący Sukces. "Tak, i..."' },
-        { min: 2, description: 'Sukces. "Tak."' },
-        { min: 0, description: 'Znikomy Sukces. "Tak, ale..."' },
-        { min: -1, description: 'Znikoma Porażka. "Nie, ale..."' },
-        { min: -3, description: 'Porażka. "Nie."' },
-        { min: -5, description: 'Krytyczna Porażka. "Nie, i..."' },
-        { min: -Infinity, description: 'Zdumiewająca Porażka. "Nie, nie ma mowy!"' }
-    ];
-    
-    testData.result.description = descriptions.find(d => SL >= d.min).description;
-    
-}
-
-Hooks.on('wfrp4e:rollTest', chudy_hook);
-alert('Szczęśliwe kości Chudego zainstalowano pomyślnie');
